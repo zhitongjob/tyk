a9272fc525cd05583daa1892114ad5f52bdc1096
Martin Buhr <martin@tyk.io>
2017-09-15 08:37:25 +1200

    Added build script

---
+++ utils/build_for_plugins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+mkdir -p /go
+mv vendor _novendor
+ln -s $PWD/_novendor /go/src
+ln -s /develop/go/src/github.com/TykTechnologies/tyk /go/src/github.com/TykTechnologies/tyk
+OLD_GOPATH=$GOPATH
+export GOPATH=/go
+go build
+export GOPATH=$OLD_GOPATH
+rm /go/src/github.com/TykTechnologies/tyk
+rm -rf /go
+mv $PWD/_novendor $PWD/vendor
